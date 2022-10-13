766264529cd9b3f7dac76c90460212b146046ef1
fengling68 <lvjianping_268@163.com>
2022-10-13 14:43:07 +0000

    更新博客

---
+++ blogs/assets/04-HTML基础.html.ca16edd4.js
@@ -0,0 +1,63 @@
+import{_ as t,o as e,c as a,f as r}from"./app.f94e1640.js";const d={},n=r(`<h1 id="html\u57FA\u78402" tabindex="-1"><a class="header-anchor" href="#html\u57FA\u78402" aria-hidden="true">#</a> HTML\u57FA\u78402</h1><h2 id="\u4EC0\u4E48\u662Furl" tabindex="-1"><a class="header-anchor" href="#\u4EC0\u4E48\u662Furl" aria-hidden="true">#</a> \u4EC0\u4E48\u662FURL</h2><pre><code>URL\uFF08Uniform Resource Locator\uFF09\uFF1A\u7EDF\u4E00\u8D44\u6E90\u5B9A\u4F4D\u5668\uFF0C\u8DEF\u5F84
+URL\u4F5C\u7528\uFF1A\u7528\u6765\u6807\u8BC6\u7F51\u7EDC\u4E2D\u7684\u8D44\u6E90\u4F4D\u7F6E
+URL\u5F62\u5F0F\uFF1A\u7EDD\u5BF9\u8DEF\u5F84\uFF08\u5B8C\u6574\u5730URL\uFF0C\u5F88\u5C11\u7528\uFF09\uFF0C\u76F8\u5BF9\u8DEF\u5F84
+</code></pre><h2 id="\u76F8\u5BF9\u8DEF\u5F84" tabindex="-1"><a class="header-anchor" href="#\u76F8\u5BF9\u8DEF\u5F84" aria-hidden="true">#</a> \u76F8\u5BF9\u8DEF\u5F84</h2><pre><code>\u540C\u7EA7\u76EE\u5F55\uFF08\u76F4\u63A5\u627E\uFF09logo.png
+\u5B50\u96C6\u76EE\u5F55\uFF08\u5148\u8FDB\u5165\uFF09images/logo.png
+\u7236\u7EA7\u76EE\u5F55\uFF08\u5148\u8FD4\u56DE\uFF09../../images/logo.png \u8FD4\u56DE\u4E0A\u4E00\u7EA7
+</code></pre><h2 id="\u56FE\u50CF\u683C\u5F0F" tabindex="-1"><a class="header-anchor" href="#\u56FE\u50CF\u683C\u5F0F" aria-hidden="true">#</a> \u56FE\u50CF\u683C\u5F0F</h2><pre><code>jpg\u3001jpeg    \u91C7\u7528\u6709\u635F\u538B\u7F29\uFF0C\u538B\u7F29\u8F83\u5927\uFF1B
+gif        \u6709\u635F\u538B\u7F29\uFF0C\u6709\u8F83\u5927\u5931\u771F\uFF0C\u652F\u6301\u52A8\u753B\u548C\u900F\u660E\u56FE\u50CF\uFF1B
+png        \u65E0\u635F\u538B\u7F29\uFF0C\u652F\u6301\u900F\u660E\u8272\uFF0C\u8272\u5F69\u4E30\u5BCC\u3002
+</code></pre><blockquote><p>\u6CE8\u610F\uFF1A\u524D\u7AEF\u5207\u56FE\u901A\u5E38\u4FDD\u5B58\u6210PNG-24</p></blockquote><h2 id="\u56FE\u50CF\u6807\u7B7Eimg" tabindex="-1"><a class="header-anchor" href="#\u56FE\u50CF\u6807\u7B7Eimg" aria-hidden="true">#</a> \u56FE\u50CF\u6807\u7B7Eimg</h2><pre><code>src:\u56FE\u50CF\u8DEF\u5F84\uFF08\u5FC5\u5907\uFF09
+alt\uFF1A\u56FE\u50CF\u4E0D\u663E\u793A\u65F6\uFF0C\u663E\u793A\u7684\u6587\u672C\uFF08\u5FC5\u5907\uFF09 alt=&quot;\u5C71\u6C34\u753B&quot;
+title\uFF1A\u9F20\u6807\u79FB\u5165\u65F6\u663E\u793A\u7684\u6587\u672C
+width   height
+</code></pre><h2 id="\u94FE\u63A5a\u6807\u7B7E" tabindex="-1"><a class="header-anchor" href="#\u94FE\u63A5a\u6807\u7B7E" aria-hidden="true">#</a> \u94FE\u63A5a\u6807\u7B7E</h2><h3 id="_1\u3001\u5C5E\u6027" tabindex="-1"><a class="header-anchor" href="#_1\u3001\u5C5E\u6027" aria-hidden="true">#</a> 1\u3001\u5C5E\u6027</h3><pre><code>href\uFF1A\u94FE\u63A5\u7684\u76EE\u7684
+target\uFF1A\u6307\u5B9A\u6253\u5F00\u65B0\u7F51\u9875\u7684\u65B9\u5F0F
+    - _self\uFF1A\u5728\u5F53\u524D\u9875\u9762\u6253\u5F00\uFF08\u9ED8\u8BA4\uFF09\uFF0C\u6253\u5F00\u7684\u9875\u9762\u4F1A\u8986\u76D6\u5F53\u524D\u9875\u9762\u3002
+    - _blank\uFF1A\u5728\u65B0\u6807\u7B7E\u9875\u4E2D\u6253\u5F00\uFF0C\u4E0D\u4F1A\u8986\u76D6\u539F\u6709\u9875\u9762\u3002
+name\uFF1A\u5B9A\u4E49\u951A\u70B9\u540D\u79F0\u3002
+</code></pre><h3 id="_2\u3001\u5728\u6240\u6709\u6D4F\u89C8\u5668\u4E2D-\u94FE\u63A5\u7684\u9ED8\u8BA4\u5916\u89C2" tabindex="-1"><a class="header-anchor" href="#_2\u3001\u5728\u6240\u6709\u6D4F\u89C8\u5668\u4E2D-\u94FE\u63A5\u7684\u9ED8\u8BA4\u5916\u89C2" aria-hidden="true">#</a> 2\u3001\u5728\u6240\u6709\u6D4F\u89C8\u5668\u4E2D\uFF0C\u94FE\u63A5\u7684\u9ED8\u8BA4\u5916\u89C2</h3><pre><code>\u672A\u88AB\u8BBF\u95EE\u5E26\u4E0B\u5212\u7EBF\u84DD\u8272\uFF1B
+\u5DF2\u88AB\u8BBF\u95EE\u5E26\u4E0B\u5212\u7EBF\u7D2B\u8272\uFF1B
+\u6D3B\u52A8\u94FE\u63A5\u5E26\u4E0B\u5212\u7EBF\u7EA2\u8272\u3002
+</code></pre><h3 id="_3\u3001\u94FE\u63A5\u7684\u8868\u73B0\u5F62\u5F0F" tabindex="-1"><a class="header-anchor" href="#_3\u3001\u94FE\u63A5\u7684\u8868\u73B0\u5F62\u5F0F" aria-hidden="true">#</a> 3\u3001\u94FE\u63A5\u7684\u8868\u73B0\u5F62\u5F0F</h3><pre><code>1. \u9875\u9762\u95F4\u7684\u8DF3\u8F6C \`&lt;a href=&quot;\u9875\u9762\u8DEF\u5F84&quot;&gt; \u6587\u672C\u6216\u56FE\u50CF &lt;/a&gt;\`
+2. \u9875\u9762\u4E0A\u8D44\u6E90\u7684\u4E0B\u8F7D \`&lt;a href=&quot;xxx.rar&quot;&gt; \u70B9\u51FB\u4E0B\u8F7D &lt;/a&gt;\`
+3. \u8FD4\u56DE\u9875\u9762\u9876\u90E8\u7684\u7A7A\u94FE\u63A5 \`&lt;a href=&quot;#&quot;&gt;\u8FD4\u56DE\u9876\u90E8&lt;/a&gt;\`
+4. \u7535\u5B50\u90AE\u4EF6\u94FE\u63A5\uFF0C\u6253\u5F00\u672C\u673A\u90AE\u4EF6\u5BA2\u6237\u7AEF\uFF0C\u5411\u6307\u5B9A\u5730\u5740\u53D1\u9001email\u90AE\u4EF6(\u5982\u672A\u5B89\u88C5\u90AE\u4EF6\u5BA2\u6237\u7AEF,\u5219\u6CA1\u6709\u6548\u679C)\`&lt;a href=&quot;mailto:xxx@qq.com&quot;&gt;\u8054\u7CFB\u6211\u4EEC&lt;/a&gt;\`
+5. \u94FE\u63A5\u5230JavaScript\u5355\u53CC\u5F15\u53F7\u5D4C\u5957\`&lt;a href=&quot;javascript:window.alert(&#39;\u6211\u662F\u4E00\u4E2A\u5BF9\u8BDD\u6846&#39;)&quot;&gt;&lt;/a&gt;\`
+</code></pre><h3 id="_4\u3001\u94FE\u63A5\u951A\u70B9" tabindex="-1"><a class="header-anchor" href="#_4\u3001\u94FE\u63A5\u951A\u70B9" aria-hidden="true">#</a> 4\u3001\u94FE\u63A5\u951A\u70B9</h3><ul><li>\u951A\u70B9\uFF1A\u5728\u9875\u9762\u7684\u4EFB\u610F\u4F4D\u7F6E\u5904\u5B9A\u4E49\u4E00\u4E2A\u6807\u8BC6\uFF0C\u968F\u65F6\u968F\u5730\u90FD\u53EF\u4EE5\u8DF3\u8F6C\u5230\u8FD9\u4E2A\u6807\u8BC6\u4E0A</li></ul><ol><li><p>\u5B9A\u4E49\u951A\u70B9\uFF1A<code>&lt;a name=&quot;anchorName&quot;&gt;&lt;/a&gt;</code> \u6807\u7B7E\u5185\u90E8\u4E0D\u9700\u8981\u653E\u5185\u5BB9</p></li><li><p>\u94FE\u63A5\u5230\u672C\u9875\u9762 <code>&lt;a href=&quot;#anchorName&quot;&gt;\u6587\u672C\u6216\u56FE\u50CF&lt;/a&gt;</code><code>&lt;a name=&quot;anchorName&quot;&gt;\u6587\u672C\u6216\u56FE\u50CF&lt;/a&gt;</code></p></li><li><p>\u94FE\u63A5\u5230\u5176\u4ED6\u9875\u9762 <code>&lt;a href=&quot;url#otherName&quot;&gt;&lt;/a&gt;</code><code>&lt;a name=&quot;otherName&quot;&gt;\u6587\u672C\u6216\u56FE\u50CF&lt;/a&gt;</code></p></li></ol><h2 id="\u8868\u683C" tabindex="-1"><a class="header-anchor" href="#\u8868\u683C" aria-hidden="true">#</a> \u8868\u683C</h2><pre><code>&lt;table&gt;          &lt;!-- \u521B\u5EFA\u8868\u683C --&gt;
+    &lt;caption&gt;\u8868\u683C\u6807\u9898&lt;/caption&gt;
+    &lt;tr&gt;           &lt;!-- \u4E00\u884C --&gt;
+        &lt;td&gt;&lt;/td&gt;    &lt;!-- \u4E24\u5217 --&gt;
+        &lt;td&gt;&lt;/td&gt;
+    &lt;/tr&gt;
+&lt;/table&gt;
+</code></pre><h3 id="_1\u3001table\u7684\u5C5E\u6027" tabindex="-1"><a class="header-anchor" href="#_1\u3001table\u7684\u5C5E\u6027" aria-hidden="true">#</a> 1\u3001table\u7684\u5C5E\u6027</h3><pre><code>width   height   bgcolor   align(left right center)   border:\u8FB9\u6846\uFF0Cpx\u50CF\u7D20
+cellpadding:\u5185\u8FB9\u8DDD\uFF08\u5355\u5143\u683C\u8FB9\u6846\u4E0E\u5185\u5BB9\u4E4B\u95F4\u7684\u8DDD\u79BB\uFF09
+cellspacing:\u5916\u8FB9\u8DDD\uFF08\u5355\u5143\u683C\u4E0E\u5355\u5143\u683C\u4E4B\u95F4\u7684\u8DDD\u79BB\uFF09
+</code></pre><h3 id="_2\u3001tr\u7684\u5C5E\u6027" tabindex="-1"><a class="header-anchor" href="#_2\u3001tr\u7684\u5C5E\u6027" aria-hidden="true">#</a> 2\u3001tr\u7684\u5C5E\u6027</h3><pre><code>align(left center right):\u8BBE\u7F6E\u8BE5\u884C\u5185\u5BB9\u7684\u6C34\u5E73\u5BF9\u9F50\u65B9\u5F0F    
+valign(top middle bottom):\u8BBE\u7F6E\u8BE5\u884C\u5185\u5BB9\u7684\u5782\u76F4\u5BF9\u9F50\u65B9\u5F0F
+</code></pre><h3 id="_3\u3001td\u7684\u5C5E\u6027" tabindex="-1"><a class="header-anchor" href="#_3\u3001td\u7684\u5C5E\u6027" aria-hidden="true">#</a> 3\u3001td\u7684\u5C5E\u6027</h3><pre><code>align    valign    width    height
+colspan:\u5355\u5143\u683C\u8DE8\u5217\uFF08\u5411\u53F3\u5408\u5E76\u5355\u5143\u683C\uFF09
+rowspan:\u5355\u5143\u683C\u8DE8\u884C\uFF08\u5411\u4E0B\u5408\u5E76\u5355\u5143\u683C\uFF09
+</code></pre><h3 id="_4\u3001\u8868\u683C\u7684\u590D\u6742\u5E94\u7528" tabindex="-1"><a class="header-anchor" href="#_4\u3001\u8868\u683C\u7684\u590D\u6742\u5E94\u7528" aria-hidden="true">#</a> 4\u3001\u8868\u683C\u7684\u590D\u6742\u5E94\u7528</h3><pre><code>\u6839\u636E\u884C\u7684\u4E0D\u540C\uFF0C\u5212\u5206\u6210\u4E0D\u540C\u7684\u7EC4\uFF1B\u8868\u683C\u7684\u5D4C\u5957
+\u8868\u5934&lt;thead&gt;   \u8868\u4E3B\u9898&lt;tbody&gt;   \u8868\u5C3E&lt;tfoot&gt;
+\u8868\u5934\u6807\u9898&lt;th&gt;&lt;/th&gt;
+</code></pre><h2 id="\u5217\u8868" tabindex="-1"><a class="header-anchor" href="#\u5217\u8868" aria-hidden="true">#</a> \u5217\u8868</h2><p>\u4ECE\u4E0A\u5230\u4E0B\uFF0C\u6210\u5217\uFF1B\u4ECE\u5DE6\u5230\u53F3\uFF0C\u6210\u884C</p><h3 id="_1\u3001\u6709\u5E8F\u5217\u8868-\u5355\u72EC\u6210\u884C-\u505A\u5BFC\u822A\u7528" tabindex="-1"><a class="header-anchor" href="#_1\u3001\u6709\u5E8F\u5217\u8868-\u5355\u72EC\u6210\u884C-\u505A\u5BFC\u822A\u7528" aria-hidden="true">#</a> 1\u3001\u6709\u5E8F\u5217\u8868 \u5355\u72EC\u6210\u884C\uFF0C\u505A\u5BFC\u822A\u7528</h3><pre><code>&lt;ol type=&quot;A&quot; start=&quot;2&quot;&gt;
+    &lt;li&gt;&lt;/li&gt;
+    &lt;li&gt;&lt;/li&gt;
+&lt;/ol&gt;
+
+type\u5217\u8868\u7C7B\u578B\uFF1A\u6570\u5B57(\u9ED8\u8BA4)  \u5C0F\u5199\u5B57\u6BCD   \u5927\u5199\u5B57\u6BCD   \u5C0F\u5199\u7F57\u9A6C\u6570\u5B57   \u5927\u5199\u7F57\u9A6C\u6570\u5B57
+start\u8D77\u59CB\u7F16\u53F7\uFF1A\u4E00\u5B9A\u662F\u6570\u5B57
+</code></pre><h3 id="_2\u3001\u65E0\u5E8F\u5217\u8868" tabindex="-1"><a class="header-anchor" href="#_2\u3001\u65E0\u5E8F\u5217\u8868" aria-hidden="true">#</a> 2\u3001\u65E0\u5E8F\u5217\u8868</h3><pre><code>&lt;ul type=&quot;circle&quot;&gt;
+    &lt;li&gt;&lt;/li&gt;
+    &lt;li&gt;&lt;/li&gt;
+&lt;/ul&gt;
+
+type\u5217\u8868\u7C7B\u578B\uFF1Adisc(\u5B9E\u5FC3\u5706\uFF0C\u9ED8\u8BA4)  circle(\u7A7A\u5FC3\u5706)  square(\u5B9E\u5FC3\u77E9\u5F62)
+</code></pre><h3 id="_3\u3001\u81EA\u5B9A\u4E49\u5217\u8868" tabindex="-1"><a class="header-anchor" href="#_3\u3001\u81EA\u5B9A\u4E49\u5217\u8868" aria-hidden="true">#</a> 3\u3001\u81EA\u5B9A\u4E49\u5217\u8868</h3><pre><code>\u7528\u4E8E\u7ED9\u51FA\u4E00\u7C7B\u4E8B\u7269\u7684\u89E3\u91CA\uFF0C\u5982\u540D\u8BCD\u89E3\u91CA\u3002\u505A\u56FE\u6587\u6DF7\u6392\u5E03\u5C40\u3002
+&lt;dl&gt;
+    &lt;dt&gt;\u5217\u8868\u5185\u5BB9\u6807\u9898&lt;/dt&gt;   &lt;!-- dt\u548Cdd\u4E0D\u662F\u5D4C\u5957\uFF0C\u662F\u5E76\u5217\u5173\u7CFB --&gt;
+    &lt;dd&gt;\u5217\u8868\u6570\u636E&lt;/dd&gt;
+&lt;/dl&gt;
+</code></pre><h3 id="_4\u3001\u5217\u8868\u7684\u5D4C\u5957" tabindex="-1"><a class="header-anchor" href="#_4\u3001\u5217\u8868\u7684\u5D4C\u5957" aria-hidden="true">#</a> 4\u3001\u5217\u8868\u7684\u5D4C\u5957</h3>`,39),h=[n];function l(o,i){return e(),a("div",null,h)}const g=t(d,[["render",l],["__file","04-HTML\u57FA\u7840.html.vue"]]);export{g as default};
