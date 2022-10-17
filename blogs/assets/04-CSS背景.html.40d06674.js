9fb3d1bddd4d1f0549b77362d2371add7164569e
fengling68 <lvjianping_268@163.com>
2022-10-17 00:32:27 +0000

    更新博客

---
+++ blogs/assets/04-CSS背景.html.40d06674.js
@@ -0,0 +1,46 @@
+import{_ as e,o as a,c as r,f as n}from"./app.20c26ebb.js";const d={},o=n(`<h1 id="css\u80CC\u666F" tabindex="-1"><a class="header-anchor" href="#css\u80CC\u666F" aria-hidden="true">#</a> CSS\u80CC\u666F</h1><h2 id="\u80CC\u666F\u989C\u8272" tabindex="-1"><a class="header-anchor" href="#\u80CC\u666F\u989C\u8272" aria-hidden="true">#</a> \u80CC\u666F\u989C\u8272</h2><pre><code>background-color\uFF1Ared;       background-color\u4E0D\u80FD\u7EE7\u627F
+background-color\uFF1Atransparent;   \u8BBE\u7F6E\u80CC\u666F\u4E3A\u201C\u900F\u660E\u201D
+</code></pre><h2 id="\u80CC\u666F\u56FE\u7247-\u5E38\u7528" tabindex="-1"><a class="header-anchor" href="#\u80CC\u666F\u56FE\u7247-\u5E38\u7528" aria-hidden="true">#</a> \u80CC\u666F\u56FE\u7247(\u5E38\u7528)</h2><p>\u80CC\u666F\u56FE\u7247\u53EF\u4EE5\u5199\u6587\u5B57\uFF0Cimg\u4E0D\u53EF\u4EE5</p><pre><code>background-image:url(images/logo.png);      \u65E0\u5F15\u53F7
+background-image:url(&#39;images/logo.png&#39;);    \u5355\u5F15\u53F7
+background-image:url(&quot;images/logo.png&quot;\u56FE\u50CF\u56FA\u5B9A);    \u53CC\u5F15\u53F7
+</code></pre><h2 id="\u80CC\u666F\u91CD\u590D-\u5E38\u7528" tabindex="-1"><a class="header-anchor" href="#\u80CC\u666F\u91CD\u590D-\u5E38\u7528" aria-hidden="true">#</a> \u80CC\u666F\u91CD\u590D(\u5E38\u7528)</h2><p>\u80CC\u666F\u56FE\u9ED8\u8BA4\u6C34\u5E73\u5782\u76F4\u65B9\u5411\u90FD\u5E73\u94FA\u3002</p><pre><code>background-repeat:repeat/repeat-x/repeat-y/no-repeat;
+                   \u5E73\u94FA/\u6C34\u5E73\u5E73\u94FA/\u5782\u76F4\u5E73\u94FA/\u4E0D\u5E73\u94FA
+</code></pre><h2 id="\u6539\u80CC\u666F\u56FE\u7247\u7684\u5C3A\u5BF8" tabindex="-1"><a class="header-anchor" href="#\u6539\u80CC\u666F\u56FE\u7247\u7684\u5C3A\u5BF8" aria-hidden="true">#</a> \u6539\u80CC\u666F\u56FE\u7247\u7684\u5C3A\u5BF8</h2><pre><code>background-size:100px 100px;   background-size:50% 50%;
+\u7279\u6B8A1\u3001background-size:contain;   \u6269\u5927\u80CC\u666F\u56FE\u50CF,\u78B0\u5230\u7B2C\u4E00\u4E2A\u8FB9\u7F18\u5C31\u505C\u6B62
+\u7279\u6B8A2\u3001background-size:cover;     \u6269\u5927\u80CC\u666F\u56FE\u50CF\uFF0C\u4F7F\u80CC\u666F\u56FE\u5B8C\u5168\u8986\u76D6\u80CC\u666F\u533A\u57DF\uFF0C\u76F4\u5230\u78B0\u5230\u6700\u540E\u4E00\u4E2A\u8FB9\u7F18\u624D\u505C\u6B62
+</code></pre><h2 id="\u80CC\u666F\u56FE\u50CF\u56FA\u5B9A" tabindex="-1"><a class="header-anchor" href="#\u80CC\u666F\u56FE\u50CF\u56FA\u5B9A" aria-hidden="true">#</a> \u80CC\u666F\u56FE\u50CF\u56FA\u5B9A</h2><pre><code>background-attachment:scroll;   \u9ED8\u8BA4\uFF0C\u6EDA\u52A8\u5B9A\u4F4D
+background-attachment:fixed;    \u56FA\u5B9A\u5B9A\u4F4D
+\u4E00\u65E6\u8BBE\u7F6E\u80CC\u666F\u56FE\u50CF\u56FA\u5B9A\uFF0C\u80CC\u666F\u56FE\u5C31\u4E0D\u4F1A\u968F\u7740\u6EDA\u8F6E\u6EDA\u52A8\u3002
+</code></pre><h2 id="\u80CC\u666F\u5B9A\u4F4D-\u5E38\u7528" tabindex="-1"><a class="header-anchor" href="#\u80CC\u666F\u5B9A\u4F4D-\u5E38\u7528" aria-hidden="true">#</a> \u80CC\u666F\u5B9A\u4F4D(\u5E38\u7528)</h2><p>\u80CC\u666F\u56FE\u50CF\u5728\u533A\u57DF\u4E2D\u7684\u54EA\u4E2A\u4F4D\u7F6E\u8FDB\u884C\u663E\u793A</p><pre><code>background-position:20px 20px;
+background-position:left center;        &lt;!-- left,center,right --&gt;
+
+x\u4E3A\u6B63\uFF0C\u5411\u53F3\uFF1Bx\u4E3A\u8D1F\uFF0C\u5411\u5DE6\uFF1B
+y\u4E3A\u6B63\uFF0C\u5411\u4E0B\uFF1By\u4E3A\u8D1F\uFF0C\u5411\u4E0A\uFF1B
+</code></pre><h3 id="_1\u3001\u5757\u7EA7\u5143\u7D20\u62A0\u56FE" tabindex="-1"><a class="header-anchor" href="#_1\u3001\u5757\u7EA7\u5143\u7D20\u62A0\u56FE" aria-hidden="true">#</a> 1\u3001\u5757\u7EA7\u5143\u7D20\u62A0\u56FE\uFF1A</h3><pre><code>\uFF081\uFF09ps\u91CF\u53D6\u60F3\u8981\u6263\u53D6\u7684\u5C0F\u56FE\u7684\u5BBD\u9AD8;
+\uFF082\uFF09\u521B\u5EFA\u4E00\u4E2A\u4E0E\u8981\u663E\u793A\u56FE\u6807width\u548Cheight\u5BBD\u9AD8\u4E00\u81F4\u7684\u5143\u7D20;
+\uFF083\uFF09\u5F15\u5165\u80CC\u666F\u56FE\uFF0C\u4E3A\u521B\u5EFA\u597D\u7684\u5143\u7D20\u8BBE\u7F6E\u80CC\u666F\u56FE\u50CF(\u5927\u80CC\u666F\u56FE);
+\uFF084\uFF09background-position\u7ED9\u8D1F\u503C,\u5C06\u56FE\u7247\u79FB\u5165\u5230\u6307\u5B9A\u4F4D\u7F6E\uFF0C\u901A\u8FC7\u5F00\u53D1\u8005\u5DE5\u5177\u5C06\u80CC\u666F\u56FE\u8C03\u6574\u5230div\u4E2D\u3002
+
+div{
+    width:22px;
+    height:23px;
+    border:1px solid red;
+    background-image:url(iconlist_1.png);
+    background-repeat:no-repeat;
+    background-position:-146px -143px;
+}
+</code></pre><h3 id="_2\u3001\u884C\u5185\u5143\u7D20\u62A0\u56FE" tabindex="-1"><a class="header-anchor" href="#_2\u3001\u884C\u5185\u5143\u7D20\u62A0\u56FE" aria-hidden="true">#</a> 2\u3001\u884C\u5185\u5143\u7D20\u62A0\u56FE</h3><p>\u7ED9\u884C\u5185\u5143\u7D20\u6DFB\u52A0\u6C34\u5E73\u65B9\u5411\u7684padding\uFF0C\u5143\u7D20\u4F1A\u51FA\u73B016px\u7684\u9AD8\u5EA6\uFF0C\u53EF\u4EE5\u5229\u7528\u884C\u5185\u5143\u7D20\u7684\u8FD9\u4E2A\u7279\u70B9\uFF0C\u7ED9\u884C\u5185\u5143\u7D20\u6DFB\u52A0\u80CC\u666F\u56FE\uFF01</p><p>\u4F46\u9700\u8981\u6CE8\u610F\u7684\u662F\uFF0C\u884C\u5185\u5143\u7D20\u6DFB\u52A0\u4E0A\u4E0B\u7684padding\u867D\u7136\u53EF\u4EE5\u201C\u6491\u5927\u201D\u5143\u7D20\u7684\u9AD8\u5EA6\uFF0C\u4F46\u662F\u9AD8\u5EA6\u8FC7\u5927\uFF0C\u5219\u4F1A\u51FA\u73B0\u8986\u76D6\u4E0A\u4E0B\u5143\u7D20\u7684\u60C5\u51B5</p><pre><code>s{
+padding-left:16px;
+background-image:url(iconlist_1.png);
+background-repeat:no-repeat;
+background-position:-127px -170px;
+}
+&lt;s&gt;&lt;/s&gt;
+</code></pre><h2 id="\u80CC\u666F\u7ED8\u5236\u533A\u57DF" tabindex="-1"><a class="header-anchor" href="#\u80CC\u666F\u7ED8\u5236\u533A\u57DF" aria-hidden="true">#</a> \u80CC\u666F\u7ED8\u5236\u533A\u57DF</h2><p>\u6307\u5B9A\u80CC\u666F\u53EF\u4EE5\u5728\u54EA\u4E9B\u533A\u57DF\u663E\u793A</p><pre><code>background-clip:border-box;   \u9ED8\u8BA4\u503C\uFF0C\u80CC\u666F\u88AB\u88C1\u526A\u5230\u8FB9\u6846
+background-clip:padding-box;  \u80CC\u666F\u663E\u793A\u5728padding\u548C\u5185\u5BB9\u533A\u57DF
+background-clip:center-box;   \u80CC\u666F\u663E\u793A\u5728\u5185\u5BB9\u533A\u57DF
+</code></pre><h2 id="\u80CC\u666F\u5408\u5E76" tabindex="-1"><a class="header-anchor" href="#\u80CC\u666F\u5408\u5E76" aria-hidden="true">#</a> \u80CC\u666F\u5408\u5E76</h2><pre><code>background:color url() attachment position;
+\u6839\u636E\u9700\u8981\u5408\u5E76\u3001\u6EDA\u52A8\u6761    \u548C\u56FA\u5B9A
+</code></pre><p>CSS\u89C6\u9891day02_06\u80CC\u666F(2)\u6700\u540E3\u5206\u949F\uFF0C\u56FE\u7247\u52A0\u8F7D\u6280\u5DE7</p><h2 id="\u6CE8\u610F" tabindex="-1"><a class="header-anchor" href="#\u6CE8\u610F" aria-hidden="true">#</a> \u6CE8\u610F</h2><pre><code>background:#ccc url(images4/user.png) no-repeat 5px 5px;
+\u80CC\u666F\u989C\u8272(background-color)\u8981\u653E\u5728\u80CC\u666F(background)\u56FE\u7247\u4E4B\u524D
+</code></pre>`,30),c=[o];function i(t,h){return a(),r("div",null,c)}const s=e(d,[["render",i],["__file","04-CSS\u80CC\u666F.html.vue"]]);export{s as default};
---
+++ blogs/assets/14-hilo之game.html.cc6eaa20.js
@@ -0,0 +1,166 @@
+import{_ as n,o as e,c as a,f as t}from"./app.20c26ebb.js";const i={},r=t(`<h1 id="hilo\u4E4Bgame-\u76F8\u673A\u548C\u7C92\u5B50\u7CFB\u7EDF" tabindex="-1"><a class="header-anchor" href="#hilo\u4E4Bgame-\u76F8\u673A\u548C\u7C92\u5B50\u7CFB\u7EDF" aria-hidden="true">#</a> hilo\u4E4Bgame\uFF08\u76F8\u673A\u548C\u7C92\u5B50\u7CFB\u7EDF\uFF09</h1><h2 id="_1\u3001camera-\u666E\u901A\u76F8\u673A" tabindex="-1"><a class="header-anchor" href="#_1\u3001camera-\u666E\u901A\u76F8\u673A" aria-hidden="true">#</a> 1\u3001Camera \u666E\u901A\u76F8\u673A</h2><pre><code>// \u521B\u5EFAtween\u7C7B
+var Tween = Hilo.Tween;
+
+// \u521B\u5EFA\u821E\u53F0
+var stage = new Hilo.Stage({
+    renderType: &#39;canvas&#39;,
+    container: document.getElementById(&quot;wrap&quot;),
+    width: 500,
+    height: 500
+});
+
+// \u521B\u5EFA\u5730\u56FE
+var map = new Hilo.Container({
+    width: 1000,
+    height: 1000
+}).addTo(stage);
+var bg = window.bg = new Hilo.Bitmap({
+    image: &#39;images/map.jpg&#39;,
+    width: 1000,
+    height: 1000
+}).addTo(map);
+
+// \u521B\u5EFA\u6444\u50CF\u673A\uFF0C\u8BBE\u7F6E\u8FB9\u754C\uFF0Cdeadzone
+var deadzone = [50,50,100,100];
+var camera = new Hilo.Camera({
+    width:500,
+    height:500,
+    bounds:[0, 0, 1000, 1000],      // \u6355\u6349\u8303\u56F4
+    deadzone: deadzone              // \u76F2\u533A
+});
+
+// \u663E\u793Adeadzone\u533A\u57DF
+stage.addChild(new Hilo.View({
+    x:camera.deadzone[0],
+    y:camera.deadzone[1],
+    width:camera.deadzone[2],
+    height:camera.deadzone[3],
+    background:&quot;rgba(255, 0, 0, 0.3)&quot;
+}));
+
+// \u521B\u5EFA\u7B2C\u4E00\u53EA\u9C7C
+var fish0 = new Hilo.Bitmap({
+    image: &#39;images/fish.png&#39;,
+    rect: [0, 0, 174, 126],     // \u56FE\u7247\u5927\u5C0F
+    x: 100,          // \u521D\u59CB\u4F4D\u7F6E
+    y: 100,          // \u521D\u59CB\u4F4D\u7F6E
+    scaleX: 0.5,     // \u7F29\u5C0F
+    scaleY: 0.5,     // \u7F29\u5C0F
+    pivotX: 174,     // \u9C7C\u76F8\u5BF9\u4E8Edeadzone\u7684\u4F4D\u7F6E
+    pivotY: 55,      // \u9C7C\u76F8\u5BF9\u4E8Edeadzone\u7684\u4F4D\u7F6E
+    onUpdate:function(){
+        this.x +=this.speed;
+        if(this.x &gt; 950 || this.x &lt; 0){
+            this.speed *= -1;
+            this.scaleX *= -1;
+        }
+    }
+}).addTo(map);
+fish0.speed = 2;
+
+// \u521B\u5EFA\u7B2C\u4E8C\u53EA\u9C7C
+var fish1 = new Hilo.Bitmap({
+    image: &#39;images/fish.png&#39;,
+    rect: [0, 0, 174, 126],
+    x: 100,
+    y: 100,
+    scaleX: 0.5,
+    scaleY: 0.5,
+    pivotX: 174,
+    pivotY: 55,
+    rotation:90,
+    onUpdate:function(){
+        this.y +=this.speed;
+        if(this.y &gt; 950 || this.y &lt; 0){
+            this.speed *= -1;
+            this.scaleX *= -1;
+        }
+    }
+}).addTo(map);
+fish1.speed = 2;
+
+// \u5F00\u542F\u5B9A\u65F6\u5668
+var ticker = new Hilo.Ticker(60);
+ticker.addTick(stage);
+ticker.addTick(camera);
+ticker.addTick(Tween);
+ticker.start();
+
+// map\u8DDF\u968F\u6444\u50CF\u673A\u6EDA\u52A8
+ticker.addTick({
+    tick:function(){
+        map.x = - camera.scroll.x;
+        map.y = - camera.scroll.y;
+    }
+});
+
+// \u6444\u50CF\u673A\u8DDF\u7740\u9C7C\u6EDA\u52A8
+camera.follow(fish1);
+
+// \u5207\u6362\u6444\u50CF\u673A\u8DDF\u968F\u5BF9\u8C61
+var tween;
+document.body.onclick = function(){
+    var target = camera.target == fish1?fish0:fish1;
+    // \u8981\u8DDF\u968F\u7684\u9C7C\u7684\u5750\u6807\u70B9
+    var pos = {
+        x:camera.target.x,
+        y:camera.target.y
+    };
+    // \u79FB\u9664\u4E0A\u6B21\u7684\u8DDF\u968F\u5BF9\u8C61
+    Tween.remove(tween);
+    // \u8DDF\u968F\u5F53\u524D\u7684\u9C7C
+    tween = Tween.to(pos,{
+        x:target.x,
+        y:target.y
+    },{
+        duration:500,
+        onComplete:function(){
+            camera.follow(target, deadzone);
+        }
+    })
+    // \u9C7C\u5728\u8FD0\u52A8\uFF0C\u91CD\u65B0\u8FFD\u8E2A\u4E00\u6B21
+    camera.follow(pos, null);
+}
+</code></pre><h2 id="_2\u3001camera3d-3d\u76F8\u673A-\u672A\u5B8C\u6210" tabindex="-1"><a class="header-anchor" href="#_2\u3001camera3d-3d\u76F8\u673A-\u672A\u5B8C\u6210" aria-hidden="true">#</a> 2\u3001Camera3d 3D\u76F8\u673A \uFF08\u672A\u5B8C\u6210\uFF09</h2><h2 id="_3\u3001particlesystem\u7C92\u5B50\u7CFB\u7EDF" tabindex="-1"><a class="header-anchor" href="#_3\u3001particlesystem\u7C92\u5B50\u7CFB\u7EDF" aria-hidden="true">#</a> 3\u3001ParticleSystem\u7C92\u5B50\u7CFB\u7EDF</h2><pre><code>// \u7C92\u5B50\u80CC\u666F\u56FE
+var img = document.getElementById(&quot;texture&quot;);
+// \u521B\u5EFA\u7C92\u5B50\u7CFB\u7EDF
+var particleSystem = new Hilo.ParticleSystem({
+    x:0,                             
+    y:0,
+    emitNum:5,                       // \u7C92\u5B50\u4E2A\u6570
+    emitTime:1,                      // \u65F6\u95F4\u95F4\u9694
+    particle:{                       // \u7C92\u5B50                   
+        image:img,                   // \u7C92\u5B50\u80CC\u666F\u56FE
+        life:22,
+        alphaV:-.01,
+        vxVar: 100,                   // x \u8F74\u65B9\u5411\u6C34\u5E73\u6563\u5F00
+        vyVar: 300,                   // y \u8F74\u65B9\u5411\u5782\u76F4\u6563\u5F00
+        axVar: 500,                   // z \u8F74\u6C34\u5E73\u6563\u5F00      
+        ayVar: 200,                   // z \u8F74\u5782\u76F4\u6563\u5F00
+        scale: 0.8,                   // \u7F29\u5C0F\u653E\u5927
+        rotationVar:360,              // \u65CB\u8F6C\u76F8\u5173
+        rotationVVar: 10,             // \u65CB\u8F6C\u76F8\u5173
+        pivotX:.5,                    // \u4E2D\u5FC3\u70B9
+        pivotY:.5,
+        frame:[                      // \u5E27
+            [75, 236, 7, 11],
+            [111, 160, 17, 46],
+            [68, 236, 7, 11],
+            [112, 223, 7, 17],
+            [0, 0, 30, 99]
+        ]
+    }
+});
+// \u6DFB\u52A0\u7C92\u5B50\u7CFB\u7EDF
+stage.addChild(particleSystem);
+// \u5F00\u542F\u7C92\u5B50\u7CFB\u7EDF
+particleSystem.start();
+
+// \u5728\u5B9A\u65F6\u5668\u4E2D\u8BBE\u7F6E\u7C92\u5B50\u7CFB\u7EDF\u7684\u968F\u673A\u4F4D\u7F6E
+ticker.addTick({
+    tick:function(){
+        particleSystem.emitterX = Math.random() * winWidth;
+        particleSystem.emitterY = Math.random() * winHeight;
+    }
+})
+</code></pre>`,6),d=[r];function o(c,s){return e(),a("div",null,d)}const m=n(i,[["render",o],["__file","14-hilo\u4E4Bgame.html.vue"]]);export{m as default};
